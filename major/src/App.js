ac158b636330a437050e56db3be70b05ee98b004
nakulgupta112233 <nakulgupta112233@gmail.com>
2021-07-03 18:13:44 +0530

    first commit react app to main

---
+++ major/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    < >
+      <h3>Navbar here</h3>
+      <h1>CODESCRIPT MAJOR PROJECT / home</h1>
+      <h3>Footer here</h3>
+    </>
+  );
+}
+
+export default App;
