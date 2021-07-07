23a20f6bd45b67b3e6e82f44205a4eb7c2cd5f64
nakulgupta112233 <nakulgupta112233@gmail.com>
2021-07-08 02:22:43 +0530

    login signup page

--- major/src/components/App.js
+++ major/src/components/App.js
@@ -1,12 +1,25 @@
-import './css/App.css';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./login"
+import Register from "./register"
 
 function App() {
   return (
-    < >
+    <div>
+    <div>
+      <Router>
+        <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    </div>
+    <div>
       <h3>Hi Navbar there</h3>
       <h1>CODESCRIPT MAJOR PROJECT / body</h1>
       <h3>Footer here</h3>
-    </>
+    </div>
+    </div>
   );
 }
 
